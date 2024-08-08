d53ec51a6be52c86df9ed6e4c051f95deb50a923
bigwhite <l18888852465@163.com>
2024-08-08 21:15:08 +0800

    Fix build issues1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ['https://subwaymap-git-main-bigwhiiiiites-projects.vercel.app'], 
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins.push(
+        new webpack.DefinePlugin({
+          'process.env.DEBUG': JSON.stringify(process.env.DEBUG || 'true'),
+        })
+      );
+    }
+
+    config.resolve.alias['@'] = path.resolve(__dirname);
+
+    return config;
+  },
+};
